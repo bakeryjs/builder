e293b7dd9097c852d7ae054fdb03694b0b89cc58
Yehor Bublyk <yehor.bk@gmail.com>
2022-05-17 21:41:57 +0300

    builder functionality implemented

--- test/stub.test.js
+++ test/stub.test.js
@@ -1,8 +1,43 @@
 const mocha = require('mocha');
-const assert = require('assert');
+const { assert } = require('chai');
 
-mocha.describe('Stub test', () => {
-  mocha.it('should pass', () => {
-    assert.strictEqual(true, true);
+const Builder = require('../lib/builder');
+
+mocha.describe('Builder', () => {
+  mocha.it('validate builder for object', () => {
+    const object = { foo: '' };
+    const builder = Builder(object);
+    assert.isFunction(builder.foo);
+  });
+  mocha.it('validate builder for function', () => {
+    function Foo() {
+      this.foo = '';
+    }
+    const builder = Builder(Foo);
+    assert.isFunction(builder.foo);
+  });
+  mocha.it('validate builder for class', () => {
+    class Foo {
+      constructor(foo) {
+        this.foo = foo;
+      }
+    }
+    const builder = Builder(Foo);
+    assert.isFunction(builder.foo);
+  });
+  mocha.it('validate builder methods', () => {
+    const object = {
+      fooVariable: '',
+      fooFunction: () => {},
+    };
+    const builder = Builder(object);
+    assert.isFunction(builder.fooVariable);
+    assert.isUndefined(builder.fooFunction);
+  });
+  mocha.it('validate builder result', () => {
+    const object = { foo: '' };
+    const expected = { foo: 'value' };
+    const actual = Builder(object).foo(expected.foo).build();
+    assert.deepStrictEqual(actual, expected);
   });
 });
